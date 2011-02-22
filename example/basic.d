8134a0bbf5cbe46cb5d1dd56e356cfddb8b0d3cd
Masahiro Nakagawa <repeatedly@gmail.com>
2011-02-22 13:55:39 +0900

    Add basic example

---
+++ example/basic.d
@@ -0,0 +1,17 @@
+import mustache;
+import std.stdio;
+
+alias MustacheEngine!(string) Mustache;
+
+void main()
+{
+    Mustache mustache;
+    auto context = new Mustache.Context;
+
+    context["name"]  = "Chris";
+    context["value"] = 10000;
+    context["taxed_value"] = 10000 - (10000 * 0.4);
+    context.useSection("in_ca");
+
+    write(mustache.render("example/basic", context));
+}
